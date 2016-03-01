5d791dd58ba0d2084d66bdafa4134cf9bf70811a
gdown67 <gdown67@googlemail.com>
2016-03-01 07:58:48 +0000

    nNew files added

--- app/App.js
+++ app/App.js
@@ -2,11 +2,16 @@ var React = require('react');
 
 var App = React.createClass({
 	render: function() {
-		return (
-			<h1>Hello world!</h1>
+	    return (
+            <div>
+                <h1>{this.props.now}</h1>
+                <ul>
+                    { this.props.items.map(item => <li key={item.id}>{item.name}</li>) }
+                </ul>
+			    <h1>Hello!</h1>
+            </div>
 		);
 	}
-	
 });
 	
 module.exports = App;
--- app/main.js
+++ app/main.js
@@ -1,4 +1,22 @@
 var React = require('react');
 var ReactDOM = require('react-dom');
-var App = require('./App.js');
-ReactDOM.render(<App/>, document.body);
+var App = require('./App.js'); 
+var names = require('./names.js');
+var httpClient = require('axios');
+
+function render(data) {
+    ReactDOM.render(<App items={data.items} now={Date.now()}/>, document.body);
+}
+
+function fetchData() {
+    return httpClient({ url: 'fake-api.json' });
+}
+
+function fetchDataAndRender() {
+    fetchData().then(function(resp) {
+        render(resp.data);
+    });
+}
+
+fetchDataAndRender();
+setInterval(fetchDataAndRender, 1000);
